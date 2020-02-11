3059cc8be85afc746a0a49f62ac55e5243cce196
Laurent <laurent.muller@bepatient.com>
2020-02-11 10:21:04 +0100

    renaming stuff

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,40 +1,41 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { Observable, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
 
-import { Product } from './product';
-import { Supplier } from '../suppliers/supplier';
-import { SupplierService } from '../suppliers/supplier.service';
+import { Product } from "./product";
+import { Supplier } from "../suppliers/supplier";
+import { SupplierService } from "../suppliers/supplier.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProductService {
-  private productsUrl = 'api/products';
+  private productsUrl = "api/products";
   private suppliersUrl = this.supplierService.suppliersUrl;
 
-  constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+  constructor(
+    private http: HttpClient,
+    private supplierService: SupplierService
+  ) {}
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
+    return this.http.get<Product[]>(this.productsUrl).pipe(
+      tap(data => console.table(data)),
+      catchError(this.handleError)
+    );
   }
 
   private fakeProduct() {
     return {
       id: 42,
-      productName: 'Another One',
-      productCode: 'TBX-0042',
-      description: 'Our new product',
+      productName: "Another One",
+      productCode: "TBX-0042",
+      description: "Our new product",
       price: 8.9,
       categoryId: 3,
-      category: 'Toolbox',
+      category: "Toolbox",
       quantityInStock: 30
     };
   }
@@ -54,5 +55,4 @@ export class ProductService {
     console.error(err);
     return throwError(errorMessage);
   }
-
 }
